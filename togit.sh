ba4cf234ba5524d950942f0f27b245e8e2a071b5
qixingyue <qixingyue@gmail.com>
2013-11-25 02:46:21 +0800

    commit at 2013-11-25-02_46_21

--- togit.sh
+++ togit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-current_date=`date +%Y-%m-%d-%H_%M_%s`
+current_date=`date +%Y-%m-%d-%H_%M_%S`
 current_message="commit at $current_date"
 echo "Begining upload to github"
 echo $current_message >> "commit.html"
