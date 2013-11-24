51514af229849cd9a5046f287e80e3a2f03acbb0
qixingyue <qixingyue@gmail.com>
2013-11-25 02:42:46 +0800

    commit at 20134211/25/13020246

--- togit.sh
+++ togit.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+
 echo "Begining upload to github"
 git add * 
 git add .htaccess
@@ -6,3 +8,5 @@ current_date=`date +%Y%M%D%H%I%S`
 current_message="commit at $current_date"
 git commit -m "$current_message"
 git push origin master
+
+
